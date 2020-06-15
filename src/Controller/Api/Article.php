cba62638fcdc9b544074003fcb2b45a59aeda2dc
Philip Pflästerer <philip@pflaesterer.org>
2020-06-15 14:55:31 +0200

    Added private fn; Changed Response to JsonResponse;

--- src/Controller/Api/Article.php
+++ src/Controller/Api/Article.php
@@ -2,24 +2,60 @@
 
 namespace App\Controller\Api;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class Article
 {
-		/**
-	 * Gets article date
-	 * 
-	 * @Route("/api/articles/{id}", methods={"GET", "HEAD"})
-	 * 
-	 * @return	Response	JSON-formatted article data
+	/**
+	 * Gets article data
+	 *
+	 * @Route(
+	 * 	"/api/articles/{id<\d+>?-1}",
+	 * 	name="article_get",
+	 * 	methods={"GET"})
+	 *
+	 * @return	JsonResponse	JSON-formatted article data
 	 */
-	public function get(int $id): Response
+	public function get(int $id): JsonResponse
 	{
-		return new Response(
-			'<html><body>JSON-DATA from article with id '.$id.'</body></html>'
-		);
+		if ($id == -1)
+			return $this->get_last_ten();
+
+		// fetch data
+		$data = array("id" => $id);
+
+		$response = new JsonResponse();
+		$response->setData($data);
+
+		return $response;
 	}
 
 
+	/**
+	 * Gets 10 last changed articles
+	 *
+	 * @return	JsonResponse	JSON-formatted article data
+	 */
+	private function get_last_ten(): JsonResponse
+	{
+		// fetch data
+		$data = array(
+			['id' => 10],
+			['id' => 11],
+			['id' => 12],
+			['id' => 13],
+			['id' => 14],
+			['id' => 15],
+			['id' => 16],
+			['id' => 17],
+			['id' => 18],
+			['id' => 19]
+		);
+
+		$response = new JsonResponse();
+		$response->setData($data);
+
+		return $response;
+	}
 }
